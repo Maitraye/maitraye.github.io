66ed7d4e9a7a75bc15c37f8044dad3b37cf6801e
Maitraye <maitraye.urmi@gmail.com>
2021-06-09 00:32:14 -0500

    update

--- js/resume.js
+++ js/resume.js
@@ -29,7 +29,7 @@
 
   $(function() {
     typed.typed({
-      strings: ["Researcher. Technologist. Music Enthusiast"],
+      strings: ["HCI Researcher. Travel & Music Enthusiast"],
       typeSpeed: 70,
       loop: true,
       showCursor: false
